4999f328d6351d47ccd1b2a30c0b55a611064403
belaerdos <140401957+belaerdos@users.noreply.github.com>
2026-02-07 08:25:03 +0100

    health text

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerHealth : MonoBehaviour
 {
@@ -8,15 +9,19 @@ public class PlayerHealth : MonoBehaviour
     public int maxHealth = 3;
     public int currentHealth;
 
+    [Header("UI")]
+    public TextMeshProUGUI healthText;
+
     void Start()
     {
         currentHealth = maxHealth;
+        UpdateHealthDisplay();
     }
 
     public void TakeDamage(int damage)
     {
         currentHealth -= damage;
-        Debug.Log("Player health: " + currentHealth);
+        UpdateHealthDisplay();
 
         if (currentHealth <= 0)
         {
@@ -30,5 +35,25 @@ public class PlayerHealth : MonoBehaviour
         // Respawn or game over later
 
         gameObject.SetActive(false);  // Deactivates and hides the GameObject
+    }
+
+    public void UpdateHealthDisplay()
+    {
+        if (healthText != null)
+        {
+            healthText.text = currentHealth.ToString();
+
+            // Color logic based on health
+            if (currentHealth >= 8)
+                healthText.color = Color.white;
+            else if (currentHealth >= 5)
+                healthText.color = Color.yellow;
+            else if (currentHealth >= 3)
+                healthText.color = Color.red;
+            else if (currentHealth >= 1)
+                healthText.color = Color.black;
+            else
+                healthText.color = Color.gray; // Dead state
+        }
     }
 }
